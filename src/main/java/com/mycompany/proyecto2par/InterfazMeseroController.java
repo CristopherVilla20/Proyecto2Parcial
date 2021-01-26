4f46662af4f1081892f38c2c814c77c4acfa6a00
CristopherVilla20 <cgvilla@espol.edu.ec>
2021-01-25 20:18:08 -0500

    Pequeños cambios a InterfazMesero

--- src/main/java/com/mycompany/proyecto2par/InterfazMeseroController.java
+++ src/main/java/com/mycompany/proyecto2par/InterfazMeseroController.java
@@ -5,15 +5,24 @@
  */
 package com.mycompany.proyecto2par;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -25,11 +34,7 @@ public class InterfazMeseroController implements Initializable {
     @FXML
     private Label lbMesero;
     @FXML
-    private Circle mesa3;
-    @FXML
-    private Circle mesa6;
-    @FXML
-    private Ellipse mesa13;
+    private Pane panelSuelo;
     @FXML
     private Button btnCerrarSesion;
 
@@ -39,15 +44,49 @@ public class InterfazMeseroController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        lbMesero.setText("Mesero: "+App.getUser().getNombre());    
-    
-    }    
-
-    @FXML
-    private void atenderMesa(MouseEvent event) {
-       App.setRoot("pedidos");
+        lbMesero.setText("Mesero: "+App.getUser().getNombre()); 
         
-    }
+         try {
+            List<Mesa> mesas = Mesa.cargarMesasArchivo("mesas.txt");
+            for (Mesa m : mesas) {
+                Circle c;
+                //true esta ocupada
+                if (m.getEstado()) {
+                    c = new Circle(m.getTamanio(), Color.RED);
+                } else {
+                    c = new Circle(m.getTamanio(), Color.GREEN);
+
+                }
+                Label l = new Label(m.getNumeroMesa());
+                StackPane st = new StackPane();
+                st.getChildren().addAll(c, l);
+
+                panelSuelo.getChildren().add(st);
+                st.setLayoutX(m.getUbicacion().getX());
+                st.setLayoutY(m.getUbicacion().getY());
+                
+                st.setOnMouseClicked((MouseEvent event)->{
+                    try {
+                        FXMLLoader fxml = new FXMLLoader(App.class.getResource("pedidos.fxml"));
+                        Parent root = fxml.load();
+                        Scene sc = new Scene(root);
+                        Stage stage = new Stage();
+                        stage.setScene(sc);
+                        stage.show();
+                        
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    
+                
+                });
+
+            }
+         }catch(IOException ex){
+             ex.printStackTrace();
+         }
+
+    }    
 
     @FXML
     private void cerrarSesion(MouseEvent event) {
--- src/main/java/com/mycompany/proyecto2par/PedidosController.java
+++ src/main/java/com/mycompany/proyecto2par/PedidosController.java
@@ -5,14 +5,20 @@
  */
 package com.mycompany.proyecto2par;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 /**
  * FXML Controller class
  *
@@ -29,12 +35,44 @@ public class PedidosController implements Initializable {
     private Button btnFinalizarOrden;
     @FXML
     private Button btnRegresar;
+    @FXML
+    private FlowPane panelPostres;
+    @FXML
+    private FlowPane panelBebidas;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        List<Comida> comidas = ComidaData.cargarComidasArchivo();
+            //System.out.println("lee");
+            for(Comida c: comidas){
+                String tipo = c.getTipo();
+                VBox contenedorPostres = new VBox();
+                VBox contenedorBebidas = new VBox();
+                
+                try{
+                InputStream inputImg = App.class.getResource(c.getImagen()).openStream();
+                //System.out.println("si abre");
+                ImageView imgv = new ImageView(new Image(inputImg));
+                Label lb1 = new Label(c.getNombre());
+                Label lb2 = new Label("$ " +String.valueOf(c.getPrecio()));
+                if(tipo.equals("Postre")){
+                    contenedorPostres.getChildren().addAll(imgv,lb1,lb2);
+                
+                }else{contenedorBebidas.getChildren().addAll(imgv,lb1,lb2);}
+                
+                
+                
+                }catch(Exception ex){
+                    System.out.println(c);
+                    ex.printStackTrace();
+                }
+                panelPostres.getChildren().add(contenedorPostres);
+                panelBebidas.getChildren().add(contenedorBebidas);
+                
+            }
     }    
 
     @FXML
